ac61551e7454a789eaaeb87841de345fa3b0da8d
Gisele Barbosa <giseleumbelino1238@gmail.com>
2026-08-29 22:08:41 -0300

    commit-test2

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,7 +1,3 @@
-// Preencha com os dados do seu projeto Supabase.
-// Use a URL do projeto e a PUBLISHABLE KEY.
-// Nunca coloque uma secret key ou service_role key no frontend.
-
 const SUPABASE_URL = "https://dfqblqroggjdgxddchkm.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_9bLw0J8cqV0jzalqyUTECg_Tpc66D62";
 
